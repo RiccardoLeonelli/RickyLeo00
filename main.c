cce4245321fdf874b17f448357bcae348dbff0cd
RiccardoLeonelli <56218610+RiccardoLeonelli@users.noreply.github.com>
2019-11-16 15:39:21 +0100

    .c
    #include <stdio.h>
    #include <stdlib.h>
    #define True 1
    #define False 0
    
    int main()
    {
        int max,verifica,i,j,contatore=0,primo;
        int *vettore;//dichiaro il mio array dinamico dove inserirò i miei numeri primi//
    
        printf("inserisci un valore massimo: ");
        verifica=scanf("%d",&max);//chiedo all' utente di inserire il valore entro il quale il codice troverà numeri primi//
        if (verifica!=1 || max<=0){//verifico se il valore inserito è corretto//
            printf("\n | valore inserito non corretto | \n");
            exit(1);//in caso contrario chiudo il programma//
        }
    
        vettore=NULL;//inizializzo l'array//
    
        for (i=1;i<=max;i++){//apro un ciclo che analizzerà ogni valore intero da 0 a max//
    
            for (j=1;j<=i;j++){//apro un ciclo che mi permette di verificare se il numero i è primo o no//
                if (i%j==0 && j!=1 && j!=i){
                    primo=False;
                    break;//se trova anche solo un valore (diverso da 1 o da se stesso) per cui i è divisibile esce e va al numero i successivo//
                }
                else{
                    primo=True;//se no il numero i è considerato primo//
                }
    
            }
    
            if (primo==True){//se il numero i è primo..//
    
                vettore=(int *)realloc(vettore,i*sizeof(int));//ingrandisco la capienza (in termini di memoria) del vettore//
                if (vettore==NULL){
                    printf("memoria insufficente");
                    exit(1);//se la memoria è insufficente esce dal programma//
                }
                else{
                    vettore[contatore]=i;//aggiungo il numero primo alla prima posizione disponibile del vettore//
                    contatore++;
                }
            }
        }
    
    
        printf("i numeri primi compresi tra 0 e %d sono :\n",max);
        for (i=0;i<contatore;i++){//stampo tutti i numeri primi trovati//
            printf(" %d \n",vettore[i]);
    
    
        }
        free(vettore);
    
    
    
        return 0;
    }

--- main.c
+++ main.c
@@ -1,148 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #define True 1
 #define False 0
 
 int main()
 {
-    int i,j,v,k,p;
-    char r,c;
-    char tab[10][10];
-    int n;
-    char last;
-    int casi[4]={0,0,0,0};
-    int FINE_GIOCO;
+    int max,verifica,i,j,contatore=0,primo;
+    int *vettore;//dichiaro il mio array dinamico dove inserirò i miei numeri primi//
 
-    //riempiano la matrice di punti
-    for (i=0;i<10;i++){
-        for (j=0;j<10;j++){
-            tab[i][j]='.';
-
-        }
-    }
-    for (i=0;i<10;i++){
-        printf("|");
-        for (j=0;j<10;j++){
-
-            printf(" %c |",tab[i][j]);
-        }
-        puts("");
+    printf("inserisci un valore massimo: ");
+    verifica=scanf("%d",&max);//chiedo all' utente di inserire il valore entro il quale il codice troverà numeri primi//
+    if (verifica!=1 || max<=0){//verifico se il valore inserito è corretto//
+        printf("\n | valore inserito non corretto | \n");
+        exit(1);//in caso contrario chiudo il programma//
     }
 
-    srand(time(NULL));
-    r=(rand()%(9-0+1))+0;
-    c=(rand()%(9-0+1))+0;
-    tab[r][c]=65;
+    vettore=NULL;//inizializzo l'array//
 
+    for (i=1;i<=max;i++){//apro un ciclo che analizzerà ogni valore intero da 0 a max//
 
-    for (last=65;last!='Z';last++){
-
-
-        while(1){
-
-            n=(rand()%(4-1+1))+1;
-
-            switch (n){
-                case 1:
-                    if (tab[r-1][c]=='.' && r!=0){
-                        r--;
-                        v=True;
-                        break;
-                    }
-                    else{
-                        v=False;
-                        casi[0]=1;
-
-                    }
-                case 2:
-                    if (tab[r][c+1]=='.' && c!=9){
-                        c++;
-                        v=True;
-                        break;
-                    }
-                    else{
-                        v=False;
-                        casi[1]=2;
-                    }
-                case 3:
-                    if (tab[r+1][c]=='.' && r!=9){
-                        r++;
-                        v=True;
-                        break;
-                    }
-                    else{
-                        v=False;
-                        casi[2]=3;
-                    }
-                case 4:
-                    if (tab[r][c-1]=='.' && c!=0){
-                        c--;
-                        v=True;
-                        break;
-                    }
-                    else{
-                        v=False;
-                        casi[3]=4;
-                    }
-                default:break;
-
-
-            }
-            if (v==True){
-                for (k=0;k<4;k++){
-                    casi[k]=0;
-                }
-
-                break;
-
+        for (j=1;j<=i;j++){//apro un ciclo che mi permette di verificare se il numero i è primo o no//
+            if (i%j==0 && j!=1 && j!=i){
+                primo=False;
+                break;//se trova anche solo un valore (diverso da 1 o da se stesso) per cui i è divisibile esce e va al numero i successivo//
             }
             else{
-                p=0;
-                for (k=0;k<4;k++){
-                    if (casi[k]==k+1){
-                        p++;
-
-
-                    }
-
-
-                }
-                if (p==4){
-                    FINE_GIOCO=True;
-                    break;
-                }
-                continue;
+                primo=True;//se no il numero i è considerato primo//
             }
 
+        }
 
+        if (primo==True){//se il numero i è primo..//
 
+            vettore=(int *)realloc(vettore,i*sizeof(int));//ingrandisco la capienza (in termini di memoria) del vettore//
+            if (vettore==NULL){
+                printf("memoria insufficente");
+                exit(1);//se la memoria è insufficente esce dal programma//
             }
-        if (FINE_GIOCO==True){
-            printf("Il gioco e' terminato prima della fine dell ' alfabeto");
-            break;
+            else{
+                vettore[contatore]=i;//aggiungo il numero primo alla prima posizione disponibile del vettore//
+                contatore++;
+            }
         }
-        tab[r][c]=last+1;
-
-        }
-
-
-
-    printf("\n\n");
-    for (i=0;i<10;i++){
-        printf("|");
-        for (j=0;j<10;j++){
-
-            printf(" %c |",tab[i][j]);
-        }
-        puts("");
     }
 
 
+    printf("i numeri primi compresi tra 0 e %d sono :\n",max);
+    for (i=0;i<contatore;i++){//stampo tutti i numeri primi trovati//
+        printf(" %d \n",vettore[i]);
 
+
+    }
+    free(vettore);
 
 
 
     return 0;
 }
-
